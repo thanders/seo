89648d05ff4056f700482a8f4d5119c60aece701
thanders <6756407+thanders@users.noreply.github.com>
2023-10-31 20:03:56 -0700

    feat(article): Article schema and initial setup

---
+++ mod.ts
@@ -0,0 +1,20 @@
+import Article from "./src/types/article";
+import SchemaMarkup from "./src/types/schemaMarkup";
+
+function generateMarkup(schemaType: string, data: SchemaMarkup): Article | null {
+    if(schemaType === 'article') {
+        const article: Article = {
+            "@context": "https://schema.org",
+            "@type": "NewsArticle",
+            "headline": "Title of a News Article",
+            author: data.author,
+            url: data.url,
+            image: [],
+            datePublished: ''
+        }
+        return article
+    }
+  return null;
+}
+
+export { generateMarkup }
