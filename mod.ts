1b0def3a6dcb14f58e4b9baaab746897ab315a0f
thanders <6756407+thanders@users.noreply.github.com>
2023-11-01 00:49:37 -0700

    refactor(generateMarkup): small refactor and changes to README

--- mod.ts
+++ mod.ts
@@ -1,20 +1,5 @@
-import ArticleSchema from "./src/types/articleSchema.ts";
-import SchemaProps from "./src/types/SchemaProps.ts";
+import ArticleProps from "./src/types/articleProps.ts";
+import generateMarkup from './src/generateMarkup.ts'
 
-function generateMarkup(schemaType: string, data: SchemaProps): ArticleSchema | null {
-    if(schemaType === 'article') {
-        const article: ArticleSchema = {
-            "@context": "https://schema.org",
-            "@type": "NewsArticle",
-            "headline": "Title of a News Article",
-            author: data.author,
-            url: data.url,
-            image: [],
-            datePublished: ''
-        }
-        return article
-    }
-  return null;
-}
-
-export { generateMarkup }
+export { generateMarkup };
+export type { ArticleProps };
